2b6c77206b6ec2b0416e6541a2c6113f38016238
Nithin G <79520840+2nithing@users.noreply.github.com>
2024-02-03 18:22:40 +0000

    f

---
+++ streamlit_app.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from streamlit_drawable_canvas import st_canvas
+from tensorflow.keras.models import load_model
+from PIL import Image
+import numpy as np
+
+@st.cache_resource
+def load_ml_model():
+    return load_model('digit_recogniser.h5')
+
+
+drawing_mode =  "freedraw"
+
+stroke_width = 20
+
+stroke_color = "#ffffff"
+bg_color = "#000000"
+
+canvas_result = st_canvas(
+    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+    stroke_width=stroke_width,
+    stroke_color=stroke_color,
+    background_color=bg_color,
+    background_image=None,
+    height=250,
+    width=250,
+    drawing_mode=drawing_mode,
+    point_display_radius= 0,
+    key="canvas",
+)
+
+if canvas_result.image_data is not None:
+    st.image(canvas_result.image_data)
+    print(type(canvas_result.image_data))
+    im = Image.fromarray(canvas_result.image_data)
+    im= im.convert("L")
+    im = im.resize((28,28))
+    st.image(im)
+    model = load_ml_model()
+    print(np.array(im).shape)
+    prediction = model.predict(np.array(im).reshape(1,28,28,1))
+    st.write("Predicted result:")
+    st.title(np.argmax(prediction))
